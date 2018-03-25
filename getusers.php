6c6f1421f3a651ae24dfb10bd065c16fd0f00a19
kengpz <keng_pz@hotmail.com>
2018-03-26 01:04:40 +0700

    s

--- getusers.php
+++ getusers.php
@@ -7,16 +7,13 @@
 $query = 'SELECT * FROM users';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
-// Printing results in HTML
-echo "<table>\n";
-while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-    echo "\t<tr>\n";
-    foreach ($line as $col_value) {
-        echo "\t\t<td>$col_value</td>\n";
-    }
-    echo "\t</tr>\n";
+// Printing results in HTML=
+ $coursesArray = array();
+	while ($row = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+		coursesArray[] = $row;
+	}
+	echo json_encode($coursesArray);
 }
-echo "</table>\n";
 
 // Free resultset
 pg_free_result($result);
