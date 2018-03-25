efaffec94f3063571d832b2d1e99132dd7f9698f
kengpz <keng_pz@hotmail.com>
2018-03-26 01:27:31 +0700

    Update getusers.php

--- getusers.php
+++ getusers.php
@@ -13,7 +13,7 @@ while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
     echo "\t\t<tr>\n";
     $i = 0;
     foreach ($line as $col_value) {
-        echo "\t\t\t\t<td>$i . $col_value</td>\n";
+        echo "\t\t\t\t<td>$col_value</td>\n";
         $i++;
     }
     echo "\t\t</tr>\n";
