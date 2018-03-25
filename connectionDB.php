7740116f2ae86e9f090465c753f1b79c62565c8b
kengpz <keng_pz@hotmail.com>
2018-03-26 02:16:09 +0700

    Create connectionDB.php

---
+++ connectionDB.php
@@ -0,0 +1,6 @@
+<?php
+// credentials
+require("pl_credentials.php");
+// Connecting, selecting database
+$dbconn = pg_connect("$host $dbname $user $password") or die('Could not connect: ' . pg_last_error());
+?>
