410f8b9b3a507b591ec77a81658aa28c81a9e015
kengpz <keng_pz@hotmail.com>
2018-03-26 02:14:43 +0700

    Update getusers.php

--- getusers.php
+++ getusers.php
@@ -1,6 +1,7 @@
 <?php
 // Connecting, selecting database
-$dbconn = pg_connect("host=ec2-54-243-130-33.compute-1.amazonaws.com dbname=d55plguu27h70h user=tdybpzwmmffehg password=52eaa707ede4a48a7a1dfe5d9d160fc07e7a3fe2a84597f0c53fdef040bb7d85") or die('Could not connect: ' . pg_last_error());
+require("connectionDB.php");
+
 // Performing SQL query
 $query = 'SELECT * FROM users';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
