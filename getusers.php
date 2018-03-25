6f8433839ba7961e677246601491b2df9e7e32e1
kengpz <keng_pz@hotmail.com>
2018-03-26 01:56:38 +0700

    Update getusers.php

--- getusers.php
+++ getusers.php
@@ -1,23 +1,24 @@
 <?php
 // Connecting, selecting database
- $dbconn = pg_connect("host=ec2-54-243-130-33.compute-1.amazonaws.com dbname=d55plguu27h70h user=tdybpzwmmffehg password=52eaa707ede4a48a7a1dfe5d9d160fc07e7a3fe2a84597f0c53fdef040bb7d85")
-    or die('Could not connect: ' . pg_last_error());
-
+$dbconn = pg_connect("host=ec2-54-243-130-33.compute-1.amazonaws.com dbname=d55plguu27h70h user=tdybpzwmmffehg password=52eaa707ede4a48a7a1dfe5d9d160fc07e7a3fe2a84597f0c53fdef040bb7d85") or die('Could not connect: ' . pg_last_error());
 // Performing SQL query
 $query = 'SELECT * FROM users';
 $result = pg_query($query) or die('Query failed: ' . pg_last_error());
-
 // Printing results in HTML
 $data = array();
 while ($row = pg_fetch_assoc($result)) {
-      array_push($data, array('id' => $row['id']),'username' => $row['username'],'name' => $row['name'],'password' => $row['password'],'email' => $row['email']);
+    array_push($data, array(
+        'id' => $row['id'],
+        'username' => $row['username'],
+        'name' => $row['name'],
+        'password' => $row['password'],
+        'email' => $row['email']
+    ));
 }
-    $json = json_encode($data);
-    echo $json;
+$json = json_encode($data);
+echo $json;
 // Free resultset
 pg_free_result($result);
-
 // Closing connection
-pg_close($dbconn); 
-
+pg_close($dbconn);
 ?>
