50c66e76aecb2b240fc8fcbb7ca60e03412d687a
kengpz <keng_pz@hotmail.com>
2018-03-26 20:31:15 +0700

    set header json

--- register.php
+++ register.php
@@ -16,31 +16,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 		
 		// parse JSON
 		$users = json_decode($content, true);
-
+	
+		$action = $users['action'];
 		$username = $users['$username'];
 		$name = $users['name'];
 		$password = $users['password'];
 		$email    = $users['email'];
 		
-		//check duplicate $email
-		$sql_search     = "SELECT email FROM users WHERE email = '$email';";
-		$rearch_result  = pg_query($dbconn, $sql_search);
-		$rowcount = pg_num_rows($rearch_result);
-		if ($rowcount == 1) {
-		echo json_encode(['status' => 'error','message' => "ไม่สามารถลงทะเบียนได้ อีเมลนี้มีผู้ใช้แล้ว"]);
-		exit;
+		// check action
+		if(strcmp($action, 'register') == 0){
+			//check duplicate $email
+			$sql_search     = "SELECT email FROM users WHERE email = '$email';";
+			$rearch_result  = pg_query($dbconn, $sql_search);
+			$rowcount = pg_num_rows($rearch_result);
+			if ($rowcount == 1) {
+				echo json_encode(['status' => 'error','message' => "ไม่สามารถลงทะเบียนได้ อีเมลนี้มีผู้ใช้แล้ว"], JSON_FORCE_OBJECT);
+			exit;
+			}
+
+			//insert data
+			$sql    = "INSERT INTO USERS (USERNAME,NAME,PASSWORD,EMAIL) VALUES ('$username','$name','$password','$email');";
+			$result = pg_query($dbconn, $sql);
+
+			if ($result) {
+				echo json_encode(['status' => 'ok','message' => "บันทึกข้อมูลเรียบร้อย"], JSON_FORCE_OBJECT);
+			} else {
+				echo json_encode(['status' => 'error','message' => "เกิดข้อผิดพลาดในการบันทึกข้อมูล"], JSON_FORCE_OBJECT);
+			}
 		}
-		
-		//insert data
-		$sql    = "INSERT INTO USERS (USERNAME,NAME,PASSWORD,EMAIL) VALUES ('$username','$name','$password','$email');";
-		$result = pg_query($dbconn, $sql);
-		
-		if ($result) {
-		echo json_encode(['status' => 'ok','message' => "บันทึกข้อมูลเรียบร้อย"]);
-		} else {
-		echo json_encode(['status' => 'error','message' => "เกิดข้อผิดพลาดในการบันทึกข้อมูล"], JSON_FORCE_OBJECT);
-		}
-	//}
 }
 pg_close($dbconn);
 ?>
