30f636436f520b9cd915c765e963971446f99f49
kengpz <keng_pz@hotmail.com>
2018-03-26 14:18:53 +0700

    update register

--- register.php
+++ register.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 require("connectionDB.php");
 pg_set_client_encoding($dbconn, "UTF8")
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+if ($_POST) {
     // check action
 	$action = $_POST['action'];
 	if(strcmp(action, 'register') == 0){
