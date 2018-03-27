9fc953ce5a875dee68a6d772305abd494a36818d
kengpz <keng_pz@hotmail.com>
2018-03-28 01:27:49 +0700

    Update register.php

--- register.php
+++ register.php
@@ -2,6 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header('Content-Type: text/html');
 require("connectionDB.php");
 pg_set_client_encoding($dbconn, "UTF8");
 
