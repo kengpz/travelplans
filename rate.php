fb28544cd3ba18bf8c6d5bd39c0fb24e919083c9
kengpz <keng_pz@hotmail.com>
2018-04-06 15:34:28 +0700

    rate

---
+++ rate.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Rate muay</title>
+	<script type="text/javascript">
+	setInterval(reload, 15000);
+	function reload() {
+		document.getElementById('iframeid').src += '';
+	}
+	</script>
+</head>
+
+<body>
+	<iframe id="iframeid" src="https://www.xn--o3cea3afbwl1da3wf0i.com/poll/open18_muay.php" frameborder="0" style="width:718px;height:500;"></iframe>
+</body>
+</html>
